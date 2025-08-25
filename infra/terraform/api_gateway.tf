5672689e8cb6035bb1542b5477ab9d9f08a22095
sunderam-tripathi <sunderamtripathi96@gmail.com>
2025-08-25 20:35:13 +0000

    fix(cf): increase time_sleep to 120s and re-enable IAM binding

--- infra/terraform/api_gateway.tf
+++ infra/terraform/api_gateway.tf
@@ -84,23 +84,22 @@ resource "google_service_account_iam_member" "build_sa_can_use_run_sa" {
 # Wait for Cloud Function to be fully ready before setting IAM policies
 resource "time_sleep" "wait_for_function_ready" {
   count           = var.allow_public_api ? 1 : 0
-  create_duration = "60s"
+  create_duration = "120s"
 
   depends_on = [google_cloudfunctions2_function.api_gateway]
 }
    
 # IAM binding to allow public access to the Cloud Function
-# Temporarily commented out to resolve persistent precondition errors
-# resource "google_cloudfunctions2_function_iam_member" "public_access" {
-#   count          = var.allow_public_api ? 1 : 0
-#   project        = google_cloudfunctions2_function.api_gateway[0].project
-#   location       = google_cloudfunctions2_function.api_gateway[0].location
-#   cloud_function = google_cloudfunctions2_function.api_gateway[0].name
-#   role           = "roles/cloudfunctions.invoker"
-#   member         = "allUsers"
-#
-#   depends_on = [time_sleep.wait_for_function_ready]
-# }
+resource "google_cloudfunctions2_function_iam_member" "public_access" {
+  count          = var.allow_public_api ? 1 : 0
+  project        = google_cloudfunctions2_function.api_gateway[0].project
+  location       = google_cloudfunctions2_function.api_gateway[0].location
+  cloud_function = google_cloudfunctions2_function.api_gateway[0].name
+  role           = "roles/cloudfunctions.invoker"
+  member         = "allUsers"
+
+  depends_on = [time_sleep.wait_for_function_ready]
+}
 
 # Backend service for the API gateway function
 resource "google_compute_region_network_endpoint_group" "api_gateway_neg" {
